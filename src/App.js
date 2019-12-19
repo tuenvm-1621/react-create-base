9a7847c59042b3cbed125c26aaf239c734301838
tuenvm-1621 <nguyen.viet.minh.tue@sun-asterisk.com>
2019-12-19 17:31:34 +0700

    webpack

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default () => (
+    <div>
+        WEBPACK ne
+    </div>
+)
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  watch: true,
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'output.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/env', '@babel/react']
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      favicon: './public/favicon.ico',
+      inject: true
+    }),
+  ],
+  resolve: {
+    extensions: [".json", ".js"],
+    alias: {
+      rootFolder: path.resolve(__dirname, './src'),
+    }
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    host: 'localhost',
+    hot: true,
+    port: 3000,
+    open: true,
+    onListening: server => {
+      const port = server.listeningApp.address().port;
+      console.log(`THIS APP IS RUNNING IN PORT: ${port}`);
+    }
+  }
+};
