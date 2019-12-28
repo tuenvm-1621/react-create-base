b6a7a8f5dd560de8c1027f030f6cf19955424b5c
tuenvm-1621 <nguyen.viet.minh.tue@sun-asterisk.com>
2019-12-29 00:45:38 +0700

    lint

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default () => (
-    <div>
-        WEBPACK ne
-    </div>
-)
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Child from './Child'
+
+const App = () => <Child myProp={'ESLINT'} />;
+
+export default App;
